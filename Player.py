fa205604e1d9594b2994fc7e1ed1cec532cd67b4
Your Name <irinaroldugina.ro@gmail.com>
2024-10-30 21:05:24 +0300

    UUU

--- Player.py
+++ Player.py
@@ -3,6 +3,11 @@ from deck import Deck
 class Player:
     def __init__(self,name,deck = None):
         self.name = name
-        self.deck = Deck() or deck
+        self.deck = deck if deck is not None else Deck()
+        self.health_points = 20
+        #self.deck = None
+        self.mana_pool = []
+        
     def __str__(self):
         return f'Игрок - {self.name} , Колода {self.deck}'
+    
