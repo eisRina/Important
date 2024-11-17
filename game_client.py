d79a4a961987a567db5bfa1f6f1337ace0094266
Your Name <irinaroldugina.ro@gmail.com>
2024-11-17 11:46:27 +0300

    DD

--- game_client.py
+++ game_client.py
@@ -6,15 +6,26 @@ pygame.init() # Обязательная инициализация библио
 
 # Реализуйте в этом методе создание окна размером 1280х720
 def create_screen():
-    screen_size = (1280,720)
+    screen_size = length, width = (1280, 720)
     screen = pygame.display.set_mode(screen_size)
     pygame.display.set_caption('Все будет хорощо :)')
     return screen
+
+def textures():
+    date = pygame.image.load('pictures/inter/play.png')
+    date = pygame.transform.scale(date,(130,130))
+    date_rect = date.get_rect()
+    date_rect.center = (640,360)
+    return date_rect, date
     
+
+def new_game():
+    print('ghbdndnn')
 
 # Опишите базовый цикл, который считывает и выводит на экран нажатие клавиш
 # Если нажата клавиша ESC, то окно закрывается
 def game_loop(screen):
+    my_button = textures()
     running = True
     while running:
         for event in pygame.event.get():
@@ -23,14 +34,21 @@ def game_loop(screen):
             if event.type == pygame.KEYDOWN:
                 print(f'Нажата клавиша: {pygame.key.name(event.key)}')
                 if event.key == pygame.K_ESCAPE:
-                    running = False
+                    running = False  
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if my_button[0].collidepoint(event.pos):
+                    new_game()
         screen.fill(THECOLORS['blue'])
+        screen.blit(my_button[1],my_button[0])
         pygame.display.flip()
     pygame.quit()
     sys.exit()
+    
+    
     
   
 # main функция, из которой будет запускаться игра
 if __name__ == '__main__':
   screen = create_screen()
   game_loop(screen)
+  
