137f91bb4c682e06718660e900e7933f9f0a38b9
Your Name <irinaroldugina.ro@gmail.com>
2024-12-15 12:43:34 +0300

    второй коммит

--- game_client.py
+++ game_client.py
@@ -1,8 +1,19 @@
 import pygame
 from pygame.color import THECOLORS
 import sys
+import requests
 
 pygame.init() # Обязательная инициализация библиотеки!
+
+mini_font = pygame.font.SysFont(None,24)  
+max_font = pygame.font.SysFont(None,36)
+
+
+#цВЕТА
+PINK = (255, 0, 213)
+VIOLET =(204, 0, 255)
+BLUE = (4, 0, 255)
+RED = (255, 0, 0)
 
 cards_for_game = [
     {'name': 'Golgari Grave-Troll', 'cost' : '5', 'type': 'Creature', 'strength' : '0' , 'health' : '0' },
@@ -11,6 +22,12 @@ cards_for_game = [
     {'name': 'Shamans Trance', 'cost' : '3', 'type': 'Instant', 'strength' : '' , 'health' : ''},
     {'name': 'Cabal Ritual', 'cost' : '2', 'type': 'Instant', 'strength' : '' , 'health' : ''} #найти человеков 
 ]
+
+def draw_text(text,font,colour,x,y,surface):
+    object_text = font.render(text,True,colour)
+    rect_text = object_text.get_rect()
+    rect_text.topleft = (x,y)
+    surface.blit(object_text,rect_text)
 
 # Реализуйте в этом методе создание окна размером 1280х720
 def create_screen():
@@ -60,31 +77,85 @@ def new_game():
 # Опишите базовый цикл, который считывает и выводит на экран нажатие клавиш
 # Если нажата клавиша ESC, то окно закрывается
 def game_loop(screen):
-    font = pygame.font.SysFont(None,40)
-    our_card = None
+    
+    user_name = ''
+    password = ''
+    text_box_user_name = pygame.Rect(180,100,200,30) #игр
+    text_box_password = pygame.Rect(180,135,200,30) #
+    activate_user_name = False
+    activate_password = False
+    message = ''
     
     background = loading_background()
     start_button,exit_button = textures()
     
     running = True
     while running:
+        #Фон    
+        screen.blit(background,(0,0))
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                print(f'Нажата клавиша: {pygame.key.name(event.key)}')
-                if event.key == pygame.K_ESCAPE:
-                    running = False  
+                if activate_user_name:
+                    if event.key == pygame.K_RETURN:
+                        activate_user_name = False
+                    elif event.key == pygame.K_BACKSPACE:
+                        user_name = user_name[:-1]
+                    else:
+                        user_name += event.unicode
+                elif activate_password:
+                    if event.key == pygame.K_RETURN:
+                        activate_password = False
+                    elif event.key == pygame.K_BACKSPACE:
+                        password = user_name[:-1]
+                    else:
+                        password += event.unicode
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if start_button[0].collidepoint(event.pos):
                     new_game()
-                if exit_button[0].collidepoint(event.pos):
+                elif exit_button[0].collidepoint(event.pos):
                     running = False
-            
-        screen.blit(background,(0,0))
+                elif text_box_user_name.collidepoint(event.pos):
+                    activate_user_name = True
+                    activate_password = False
+                elif text_box_password.collidepoint(event.pos):
+                    activate_user_name = False
+                    activate_password = True
+                else:
+                    activate_user_name = False
+                    activate_password = False
         
+        if pygame.mouse.get_pressed()[0] and start_button[0].collidepoint(pygame.mouse.get_pos()):
+            if len(password) > 0 and len(user_name) > 0:
+                try: 
+                    answer = requests.post('http://127.0.0.1:8000/auth', json= {'user_name': user_name,'password': password})
+                    if answer.status_code == 200: 
+                        message = f'Авторизация прошла. ID:{answer.json()["id"]}' 
+                    else:
+                        message = f'Ошибка входа. {answer.status_code}'
+                except Exception as e:
+                    message = 'Ошибка на сервере'
+            else:
+                message = f'Ошибка ввода.'
+            
+        draw_text(message, max_font, BLUE,400,150,screen)    
+        
+        
+        #Кнопки
         screen.blit(start_button[1],start_button[0])
         screen.blit(exit_button[1],exit_button[0])
+        
+        pygame.draw.rect(screen, BLUE , text_box_user_name,2)
+        draw_text(user_name,mini_font,BLUE,200,100,screen) # 
+        
+        pygame.draw.rect(screen,RED, text_box_password,2)
+        draw_text(password,mini_font,RED,200,145,screen) # 
+
+        #Текст
+        draw_text('username: ', max_font, PINK, 50, 100, screen)
+        draw_text('password: ', max_font, VIOLET, 50, 140, screen)
         
         pygame.display.flip()
     pygame.quit()
